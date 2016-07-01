d6177cad116015514d2e0961ec956e4712ec3f65
海水 <hi_beijing@126.com>
2016-07-01 16:13:26 +0800

    调整

--- js/lib/selectUI.js
+++ js/lib/selectUI.js
@@ -1,6 +1,7 @@
-;(function($, window, document, undefined ){
+;
+(function($, window, document, undefined) {
 
-  function SelectUI( jSelect, settings ) {
+  function SelectUI(jSelect, settings) {
 
     var time = +(new Date()).getTime();
 
@@ -13,29 +14,28 @@
     this.jSelect = jSelect;
     this.jOptions = this.jSelect.find("option");
     this.jOptionUI = null;
-    settings.addClass = settings.addClass || "";
 
 
     var selectHTML = "";
-    selectHTML += "<div id='"+ this.selectContainerUI +"' class=\"selectUI " + settings.addClass + "\">";
+    selectHTML += "<div id='" + this.selectContainerUI + "' class=\"jComponentSelect " + settings.addClass + "\">";
     selectHTML += "   <div id='" + this.selectTitleContainerUI + "'  class=\"jComponentSelectTitleBox\">";
     selectHTML += "     <div id='" + this.selectIconUI + "' class=\"jComponentSelectIcon\"></div>";
-    selectHTML += "     <div id='" +this.selectTitleUI + "'  class=\"jComponentSelectTitle\" ></div>";
+    selectHTML += "     <div id='" + this.selectTitleUI + "'  class=\"jComponentSelectTitle\" ></div>";
     selectHTML += "   </div>";
-    selectHTML += "   <ul id='"+ this. selectOptionListContainer +"'  class=\"jComponentSelectList\"></ul>";
+    selectHTML += "   <ul id='" + this.selectOptionListContainer + "'  class=\"jComponentSelectList\"></ul>";
     selectHTML += "</div>";
 
     // 渲染selectUI框架
     this.jSelect.before(selectHTML).hide();
 
     // selectUI Container
-    this.jSelectContainerUI = $("#" +this.selectContainerUI );
+    this.jSelectContainerUI = $("#" + this.selectContainerUI);
     // selectUI 标题修改对象
-    this.jSelectTitleUI = $("#" +this.selectTitleUI );
+    this.jSelectTitleUI = $("#" + this.selectTitleUI);
     // selectUI的下拉列表
-    this.jSelectListUI = $("#" +this.selectOptionListContainer );
+    this.jSelectListUI = $("#" + this.selectOptionListContainer);
     //icon
-    this.jSelectIconUI = $("#" + this.selectIconUI );
+    this.jSelectIconUI = $("#" + this.selectIconUI);
 
     this.init();
   }
@@ -48,16 +48,16 @@
       // 绑定事件
       this.bindEvent();
     },
-    renderHead: function( obj ) {
+    renderHead: function(obj) {
 
       var value = obj.attr("value");
       var name = obj.html();
-      var dataIndex =obj.attr("data-index");
+      var dataIndex = obj.attr("data-index");
 
       // 修改原生select的选中项
-      this.changeNativeSelect( dataIndex );
+      this.changeNativeSelect(dataIndex);
 
-      this.jSelectTitleUI.html(name).attr("title",name);
+      this.jSelectTitleUI.html(name);
 
       this.close();
     },
@@ -73,7 +73,7 @@
        *  1、给select的option 增加序号
        *  2、回调函数init　对每一个option元素的操作
        */
-      this.jOptions.map(function(i, data ){
+      this.jOptions.map(function(i, data) {
         $(data).attr("data-index", i);
         settings.init.call(this, $(data));
       });
@@ -82,51 +82,59 @@
       // 渲染selectUI组件的标题及改变打开selectUI列表时的icon状态
       this.renderHead(this.getSelectedOption());
 
-      this.jSelectContainerUI.width(settings.width).height(settings.height).css({"line-height": settings.height+"px"});
+      this.jSelectContainerUI.width(settings.width).height(settings.height).css({
+        "line-height": settings.height + "px"
+      });
       this.jSelectIconUI.width(settings.height).height(settings.height);
-      this.jSelectListUI.css({top: settings.height-1});
+      this.jSelectListUI.css({
+        top: settings.height - 1
+      });
       this.jOptions.width(settings.width).height(settings.height);
 
       // 把select中的option转换成li元素
-      var optionHTML = this.jSelect.html().replace(/(option)/gi,"li");
+      var optionHTML = this.jSelect.html().replace(/(option)/gi, "li");
 
-      this.jSelectListUI.html(optionHTML).css({"max-height": settings.height * 6});
+      this.jSelectListUI.html(optionHTML).css({
+        "max-height": settings.height * 4
+      });
 
     },
     // 修改原生select中的option
-    changeNativeSelect: function( index ) {
+    changeNativeSelect: function(index) {
       //this.jOptions.removeAttr("selected");
-      this.jOptions.eq( index ).prop("selected",true);
+      this.jOptions.eq(index).prop("selected", true);
     },
-    open: function(  ) {
+    open: function() {
       this.jSelectListUI.show();
       this.jSelectIconUI.addClass("jComponentSelectIconOpen");
     },
-    close: function(  ) {
+    close: function() {
       this.jSelectListUI.hide();
       this.jSelectIconUI.removeClass("jComponentSelectIconOpen");
     },
     bindEvent: function() {
       var that = this;
 
-/*      $(".subAccount").hover(function(){
-      $(".accountMangment").show();
-    },
-    function(){
-      $(".accountMangment").hide();
-    });*/
-    var list = $("#" + that.selectOptionListContainer);
+      /*      $(".subAccount").hover(function(){
+            $(".accountMangment").show();
+          },
+          function(){
+            $(".accountMangment").hide();
+          });*/
+      var list = $("#" + that.selectOptionListContainer);
 
-      $("#" + this.selectContainerUI ).hover(function() {
+      $("#" + this.selectTitleContainerUI).click(function() {
         that.open();
-      }, function(){
+      });
+
+      $("#" + this.selectContainerUI).hover(function() {}, function() {
         that.close();
       });
 
-      this.jSelectListUI.on("click", "li", function(){
+      this.jSelectListUI.on("click", "li", function() {
         var _that = $(this);
         var callback = that.settings.before.call(this, _that);
-        if ( callback ) {
+        if (callback) {
           that.renderHead(_that);
         }
 
@@ -144,12 +152,12 @@
     }
   }
 
-  $.fn.jComponentSelect = $.fn.selectUI = function( options ) {
+  $.fn.jComponentSelect = $.fn.selectUI = function(options) {
 
     var defaults = {
-      height:30,
-      init: function(){},
-      before:function(option){
+      height: 30,
+      init: function() {},
+      before: function(option) {
         // false是不可点的，true是可点的
         return true;
       },
@@ -160,14 +168,14 @@
     };
 
     // 如果没有找到元素就为0
-    if ($(this).size() == 0 ) {
+    if ($(this).size() == 0) {
       return {};
     }
 
-    var settings = $.extend({},defaults, options);
+    var settings = $.extend({}, defaults, options);
 
-    var selectUI = new SelectUI( $(this), settings );
+    var selectUI = new SelectUI($(this), settings);
 
     return selectUI;
   };
-})(jQuery, window, document );
+})(jQuery, window, document);
